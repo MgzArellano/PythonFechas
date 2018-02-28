6b13ad4c40ca357ae54b4ed4d65816fac36cb7f4
Marco Arellano <mgz.arellano@gmail.com>
2018-02-27 23:50:33 -0600

    Formateo de Fechas
    Formateo de fechas con python3.5

---
+++ MeasuringDurationWithTimeDelta.py
@@ -0,0 +1,5 @@
+from datetime import datetime
+
+date1 = "2017-03-03T13:51:56+00:00";
+dateObj = datetime.timestamp()
+print()
--- TrabajandoFechas.py
+++ TrabajandoFechas.py
@@ -33,5 +33,27 @@ print(mi_cumple2.strftime('%Y/%m/%d'))
 # Imprimiendo como: 07/09/1984
 print(mi_cumple2.strftime('%d/%m/%Y'))
 
-# Imprimiendo como:
-print('%s')
+# Imprimiendo como: 07 Sep 84
+print(mi_cumple2.strftime('%d %b %y'))
+
+# Imprimiendo como: 1984-09-07 09:30:00
+print(mi_cumple2.strftime('%Y-%m-%d %H:%M:%S'))
+
+# Haciéndolo de la forma anterior, podemos generar el formato deseado, indicando:
+#  %Y --> para el año
+#  %m --> para el mes
+#  %d --> para el día
+
+# Pero también es posible hacerlo indicando el nombre del atributo en inglés
+# Imprimiendo el año
+print(mi_cumple2.year)
+# Imprimiendo el mes
+print(mi_cumple2.month)
+# Imprimiendo el día
+print(mi_cumple2.day)
+# Imprimiendo la fecha completa:   1984 9 7   09:30:00
+print(mi_cumple2.year,mi_cumple2.month,mi_cumple2.day,' ',mi_cumple2.time())
+# Imprimiendo la fecha en formato ISO  :   1984-09-07T09:30:00
+print(mi_cumple2.isoformat())
+
+date1 = "2017-03-03T13:51:56+00:00";
